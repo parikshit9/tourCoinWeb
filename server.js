b1d756a8291ba95d1c1140803222612205d10ae8
Shuhail <shuhail.ahamed@edvenswa.in>
2017-11-07 14:43:52 +0530

    Changes in port

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('/home/ubuntu/ssl2/worldtourism.key', 'utf8');
+var certificate = fs.readFileSync('/home/ubuntu/ssl2/57755d0011fb26b6.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 
-var port = process.env.PORT || 9999;
+//var port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 
@@ -19,5 +26,9 @@ app.get('/', function(req, res, next) {
   res.sendfile(path.join('./public/views/index.html')); 
 });
 
-app.listen(port);
-console.log('app running on ' + port);
+//app.listen(port);
+//console.log('app running on ' + port);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpServer.listen(80);
+httpsServer.listen(443);
