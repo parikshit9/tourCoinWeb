62bc5af2eebe98c38dfa713b3136f4938672b8e9
parikshit999 <parikshit@nimbusclinic.com>
2017-11-15 02:28:31 +0530

    dashboard design and login signup api integration

--- public/js/wtoLogInCtrl.js
+++ public/js/wtoLogInCtrl.js
@@ -1,4 +1,23 @@
 worldTourApp.controller('wtoLogInCtrl', function($scope, $rootScope, $state, $timeout, $sce, $http, $window) {
 	$scope.state = $state;
 	console.log('login ctrl');
+	$scope.loginObj = {};
+
+	$scope.login = function(){
+		var postObj = angular.copy($scope.loginObj);
+
+		$http.post('http://api.worldtourism.io:8080/tourcoins/loginAuth',postObj).then(success,error);
+
+		function success(res){
+			if (res.data.status =='success') {
+				$state.go('dashboard');
+			}else if(res.data.error){
+				Materialize.toast(res.data.error, 3000);
+			}
+		}
+
+		function error(err){
+			alert(err);
+		}
+	}
 });
--- public/js/wtoSignUpCtrl.js
+++ public/js/wtoSignUpCtrl.js
@@ -1,4 +1,40 @@
 worldTourApp.controller('wtoSignUpCtrl', function($scope, $rootScope, $state, $timeout, $sce, $http, $window) {
 	$scope.state = $state;
 	console.log('signUp ctrl');
+
+	$scope.registerObj = {};
+
+	$scope.register = function(){
+		var postObj = angular.copy($scope.registerObj);
+		delete postObj.pass;
+
+		console.log(postObj);
+
+		$http.post('http://api.worldtourism.io:8080/tourcoins/createAccount',postObj).then(success,error);
+
+		function success(res){
+			console.log("res",res);
+			if (res.data.success) {
+				$state.go('verify');
+			}else if(res.data.error == "verify email"){
+				Materialize.toast("You are already registered, Please verify email", 3000);
+				$scope.registerObj = {};
+			}else{
+				Materialize.toast(res.data.error, 3000);
+				$scope.registerObj = {};
+			}
+		}
+
+		function error(err){
+			alert(err);
+		}
+	}
+
+	$scope.comparePass = function(){
+		if ($scope.registerObj.pass !== $scope.registerObj.userPassword) {
+			$scope.matchError = "Passwords don't match";
+		}else{
+			$scope.matchError = '';
+		}
+	}
 });
